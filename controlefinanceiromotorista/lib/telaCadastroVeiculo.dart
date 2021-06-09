e7af3234423cd74ca522234f4bf656e7f30e203f
Yuri Matos Rodrigues <yuri.matos@phibra.com.br>
2021-06-09 19:54:36 -0300

    Criacao TelaCadastroVeiculo

---
+++ controlefinanceiromotorista/lib/telaCadastroVeiculo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaCadastroVeiculo extends StatefulWidget {
+  @override
+  _TelaCadastroVeiculoState createState() => _TelaCadastroVeiculoState();
+}
+
+class _TelaCadastroVeiculoState extends State<TelaCadastroVeiculo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
