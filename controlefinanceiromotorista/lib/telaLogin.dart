e1c04ea248bb3602d22fd3dc8d97d26912b342da
CleitonRodriguesAraujo96 <cleitoncaico@gmail.com>
2021-06-12 03:04:51 -0300

    TelaPrincipal
    Processo de criação

--- controlefinanceiromotorista/lib/telaLogin.dart
+++ controlefinanceiromotorista/lib/telaLogin.dart
@@ -1,3 +1,5 @@
+import 'package:controlefinanceiromotorista/telaCadastro.dart';
+import 'package:controlefinanceiromotorista/telaPrincipal.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -20,86 +22,96 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 40,
-            ),
-            //Local a ser colocar o logo
-            Icon(
-              Icons.access_alarm,
-              size: 120.0,
-              color: Colors.black,
-            ),
-
-            SizedBox(
-              height: 30,
-            ),
-
-            TextField(
-              decoration: InputDecoration(
-                labelText: "LOGIN",
-                labelStyle: TextStyle(color: Colors.red),
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
               ),
-            ),
-
-            SizedBox(
-              height: 30,
-            ),
-
-            TextField(
-              decoration: InputDecoration(
-                labelText: "SENHA",
-                labelStyle: TextStyle(color: Colors.red),
-                border: OutlineInputBorder(),
+              //Local a ser colocar o logo
+              Icon(
+                Icons.account_circle,
+                size: 120.0,
+                color: Colors.black,
               ),
-            ),
 
-            SizedBox(
-              height: 30,
-            ),
+              SizedBox(
+                height: 30,
+              ),
 
-            Container(
-              width: 150,
-              height: 40,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "LOGIN",
+                  labelStyle: TextStyle(color: Colors.red),
+                  border: OutlineInputBorder(),
                 ),
-                child: Text(
-                  "LOGIN",
-                  style: TextStyle(
-                    color: Colors.white,
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "SENHA",
+                  labelStyle: TextStyle(color: Colors.red),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+
+              Container(
+                width: 150,
+                height: 40,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
                   ),
-                ),
-                onPressed: () {},
-              ),
-            ),
-
-            SizedBox(
-              height: 20,
-            ),
-
-            Container(
-              width: 150,
-              height: 40,
-              child: TextButton(
-                style: TextButton.styleFrom(backgroundColor: Colors.blue),
-                child: Text(
-                  "CRIAR CONTA",
-                  style: TextStyle(
-                    color: Colors.white,
+                  child: Text(
+                    "LOGIN",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context)=> telaPrincipal()));
+                  },
                 ),
-                onPressed: () {},
               ),
-            )
-          ],
+
+              SizedBox(
+                height: 20,
+              ),
+
+              Container(
+                width: 150,
+                height: 40,
+                child: TextButton(
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  child: Text(
+                    "CRIAR CONTA",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context)=> telaCadastro()));
+                  },
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      )
+
     );
+
   }
 }
